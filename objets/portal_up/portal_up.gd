8b8be805be6449a0879cce5385176aadf924a176
ASLAYERON <albert.boivin54@gmail.com>
2026-02-17 15:16:58 +0100

    room-to-room travel woprking
    problems with the coord label on the player

--- objets/portal_up/portal_up.gd
+++ objets/portal_up/portal_up.gd
@@ -4,7 +4,7 @@ func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player":
 		var coord_string = String(get_parent().name)
 		print(coord_string)
-		var coord_x = int(coord_string[0])+1
-		var coord_y = int(coord_string[2])
-		Global.tp_offset = Vector2(-900,body.position.y)
+		var coord_x = int(coord_string[0])
+		var coord_y = int(coord_string[2])-1
+		Global.tp_offset = Vector2(body.position.x,900)
 		get_tree().change_scene_to_file(Global.levels_map[Global.active_level][coord_y][coord_x]+"room.tscn")
